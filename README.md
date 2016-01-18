6debf32803620851a51c18537698446a8563fba4
Aaron Bartholomew <aaron@dzap.net>
2016-01-17 22:52:01 -0800

    Base commit for API files.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # qa-minion
-NodeJS tool using Express and Cheerio that runs through lists of test cases to automate webpage QA passes, 
+NodeJS tool using Express and Cheerio that runs through lists of test cases to automate webpage QA passes. Test passes are done through an API endpoint.
+
+##To Install
+`npm install`
+
+##To Run
+`node selfQA`
+
+##Test Case Endpoints
+`http://localhost:8081/?q=http://yourdomain.com/pagetotest.html`
+
+###To Do
+> Add Basic Web UI Interface
+> Test Case Looping
+> More Test Cases
+> Break Test Cases into seperate files
---
+++ selfQA.js
@@ -0,0 +1,55 @@
+/* LOAD DEPENDENCIES */
+var express = require('express');
+var request = require('request');
+var cheerio	= require('cheerio');
+var path 	= require('path');
+var app 	= new express();
+
+/* CONFIGURATION SETUP */
+var config	= {
+	port: 8081
+};
+
+/* BASE HELPER METHODS */
+var methods = {
+	stripTags: function(str) {
+    	return String(str).replace(/<\/?[^>]+(>|$)/g, '');
+	}
+}
+
+var testConfig = {
+	general : {
+		googleAnalytics : {
+			name: "Google Analytics Code Exists",
+			test: function(str) {
+				return str.indexOf("['GoogleAnalyticsObject']") > -1 ? true : false;
+			}
+		}
+	}
+}
+
+/* WEB FOLDER EXISTS TO ADD FRONTEND CODE FOR A UI */
+app.get('/web*', function(req, res) {
+	thingToGet = req["originalUrl"];
+    res.sendFile(path.join(__dirname + thingToGet));
+});
+
+/* RETURN PASSES ON PAGENAME THROUGH A JSON API */
+app.get('/', function(req, res){
+	query = req.query.q ? req.query.q : "http://aaronbartholomew.com/";
+	output = {
+		results: [],
+		string: ""
+	};
+	request(query, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+		}
+		res.json(testConfig.general.googleAnalytics.test(html));
+	});
+});
+
+/* LISTEN ON PORT */
+app.listen(config.port);
+console.log('Self QA Assistant Started on Port 8081');
+exports = module.exports = app;
