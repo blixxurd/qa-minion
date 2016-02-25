7e9d124edd6d87dedffa065f6cb3b545207dfff5
Ryan Pearson <rayn@ryanpearson.net>
2016-02-25 13:01:42 -0800

    Refactors for efficiency

--- selfQA.js
+++ selfQA.js
@@ -1,23 +1,21 @@
 /* LOAD DEPENDENCIES */
-var express = require('express');
-var request = require('request');
-var cheerio	= require('cheerio');
-var path 	= require('path');
-var app 	= new express();
+var express = require('express'),
+		request = require('request'),
+		cheerio	= require('cheerio'),
+		path 	= require('path'),
+		app 	= new express(),
 
 /* CONFIGURATION SETUP */
-var config	= {
-	port: 8081
-};
+		config	= {port: 8081},
 
 /* BASE HELPER METHODS */
-var methods = {
+		methods = {
 	stripTags: function(str) {
     	return String(str).replace(/<\/?[^>]+(>|$)/g, '');
 	}
-}
+},
 
-var testConfig = function(html, $) {
+		testConfig = function(html, $) {
 	return {
 		general : {
 			googleAnalytics : {
@@ -39,30 +37,32 @@ var testConfig = function(html, $) {
 			}
 		}
 	}
-}
+};
 
 /* WEB FOLDER EXISTS TO ADD FRONTEND CODE FOR A UI */
-app.get('/web*', function(req, res) {
-	thingToGet = req["originalUrl"];
-    res.sendFile(path.join(__dirname + thingToGet));
-});
+app
+	.get('/web*', function(req, res) {
+		thingToGet = req["originalUrl"];
+			res.sendFile(path.join(__dirname + thingToGet));
+	})
 
 /* RETURN PASSES ON PAGENAME THROUGH A JSON API */
-app.get('/', function(req, res){
-	query = req.query.q ? req.query.q : "http://aaronbartholomew.com/";
-	output = {
-		results: [],
-		string: ""
-	};
-	request(query, function(error, response, html){
-		if(!error){
-			var $ = cheerio.load(html);
-		}
-		res.json(testConfig(html, $));
-	});
-});
+	.get('/', function(req, res){
+		query = req.query.q ? req.query.q : "http://aaronbartholomew.com/";
+		output = {
+			results: [],
+			string: ""
+		};
+		request(query, function(error, response, html){
+			if(!error){
+				var $ = cheerio.load(html);
+			}
+			res.json(testConfig(html, $));
+		});
+	})
+
 
 /* LISTEN ON PORT */
-app.listen(config.port);
+	.listen(config.port);
 console.log('Self QA Assistant Started on Port 8081');
 exports = module.exports = app;
