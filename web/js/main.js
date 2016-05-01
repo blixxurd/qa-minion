8514ed99b202c363a0b0f00b430e51fbf07e0ef8
Aaron Bartholomew <aaronwinslol@gmail.com>
2016-04-30 22:53:22 -0700

    Adds basic UI components for running test from webpage.

---
+++ web/js/main.js
@@ -0,0 +1,35 @@
+/* 
+	INITIALIZE SELFQA WRAPPER
+	Dependencies: None
+*/
+var apiUi = angular.module("apiUi", []);
+
+apiUi.config(function() {});
+
+apiUi.run(function() {
+
+});
+
+apiUi.controller('formCtrl', function($scope,$http) {
+
+	var methods = {
+		get : function() {
+			$scope.error_message = "";
+			if($scope.url.indexOf("://") == -1 || $scope.url == "") {
+				$scope.error_message = "Not a properly formatted URL.";
+				return;
+			}
+			var req = $http.get("/?q="+$scope.url);
+			req.success(function(data, status, headers, config) {
+				$scope.test_data = data;
+			});
+			req.error(function(data, status, headers, config) {
+				$scope.error_message = "Something went wrong.";
+			});
+		}
+	}
+
+	$scope.processing = false;
+	$scope.methods = methods;
+
+});
